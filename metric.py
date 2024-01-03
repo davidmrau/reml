fix llama nans with bfloat16, fix print function, add precision & recall

metric.py
@@ -52,18 +52,18 @@ def normalize(s: str) -> str:
 
 
 
-def f1(prediction, ground_truth):
+def f1_single(prediction, ground_truth):
     prediction_tokens = prediction.split()
     ground_truth_tokens = ground_truth.split()
     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
     num_same = sum(common.values())
 
     if num_same == 0:
-        return 0
+        return 0, 0, 0
     precision = 1.0 * num_same / len(prediction_tokens)
     recall = 1.0 * num_same / len(ground_truth_tokens)
     f1 = (2 * precision * recall) / (precision + recall)
-    return f1
+    return f1, precision, recall
 
 def rouge_wrapper(prediction, ground_truth):
     try:
@@ -95,30 +95,41 @@ def rouge_score(predictions, references):
 
 
 def f1_score(predictions, references):
-    return np.mean([max([f1(prediction, gt) for gt in ground_truths]) for ground_truths, prediction in zip(references, predictions)])
+    f1, precision, recall = list(), list(), list()
+    for ground_truths, prediction in zip(references, predictions):
+        print(ground_truths, prediction)
+        f1_, precision_, recall_ = [max(values) for values in zip(*[f1_single(prediction, gt) for gt in ground_truths])]
+        print(f1_, precision_, recall_)
+        f1.append(f1_)
+        precision.append(precision_)
+        recall.append(recall_)
+    return np.mean(f1), np.mean(precision), np.mean(recall)
 
-def em(prediction, ground_truth):
+def em_single(prediction, ground_truth):
     return float(normalize(prediction) == normalize(ground_truth))
 
 
 def exact_match_score(predictions, references):
-    return np.mean([max([em(prediction, gt) for gt in ground_truths]) for ground_truths, prediction in zip(references, predictions)])
+    return np.mean([max([em_single(prediction, gt) for gt in ground_truths]) for ground_truths, prediction in zip(references, predictions)])
 
 
 class Metrics:
     def __init__(self, dataset_name, bem=True):
         self.dataset_name = dataset_name
-        #if bem:
+        # if bem:
         #    from evaluation.bem import BEM
         #    self.bem = BEM()
 
     def compute(self, predictions, references, questions=None):
-        if self.dataset_name == "nq_open":
+        if "nq_open" in self.dataset_name:
             rouge1, rouge2, rougel = rouge_score(predictions, references)
+            f1, precision, recall = f1_score(predictions, references)
             return {
                         "EM": exact_match_score(predictions, references),
                         #"BEM": self.bem(references, predictions, questions),
-                        "f1": f1_score(predictions, references),
+                        "F1": f1,
+                        "Precision": precision, 
+                        "Recall": recall,
                         "Rouge-1": rouge1,
                         "Rouge-2": rouge2,
                         "Rouge-L": rougel,

models/generators/llama2.py
@@ -16,13 +16,14 @@ class Llama2():
         quant_config = BitsAndBytesConfig(
                             load_in_4bit=True,
                             bnb_4bit_quant_type='nf4',
-                            bnb_4bit_compute_dtype='float16',
+                            bnb_4bit_compute_dtype='bfloat16',
                             bnb_4bit_use_dobule_quant=True
                         )
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         #self.model = AutoModelForCausalLM.from_pretrained(self.model_name, device_map='auto', quantization_config=quant_config, use_flash_attention_2=True)
         self.model = AutoModelForCausalLM.from_pretrained(self.model_name, device_map='auto', quantization_config=quant_config)
+        self.model = self.model.bfloat16()
         self.model.eval()
         self.model.config.pretraining_tp = 1
         self.max_new_tokens = max_new_tokens

rag.py
@@ -32,22 +32,22 @@ class RAG:
         self.processing_num_proc = processing_num_proc
         self.index_folder = index_folder
         self.dataset_config = dataset_config
+
+        # process datasets, downloading, loading, covert to format
+        self.datasets = ProcessDatasets.process(
+            dataset_config, 
+            out_folder=self.dataset_folder, 
+            num_proc=processing_num_proc,
+            overwrite=overwrite_datasets,
+            debug=debug,
+            )
         self.metrics = {
             "train": None,
             # lookup metric with dataset name (tuple: dataset_name, split) 
-            "test": Metrics(dataset_config['test']['query'][0]), 
+            "test": Metrics(self.datasets['test']['query'].name), 
             "dev": None
         }
 
-        with torch.no_grad(): 
-            # process datasets, downloading, loading, covert to format
-            self.datasets = ProcessDatasets.process(
-                dataset_config, 
-                out_folder=self.dataset_folder, 
-                num_proc=processing_num_proc,
-                overwrite=overwrite_datasets,
-                debug=debug,
-                )
 
         print_rag_model(self, retriever_config,reranker_config, generator_config)
         # init modules
@@ -70,7 +70,7 @@ class RAG:
 
     def default(self, split):
         fetch_pyserini_docs=False
-        dataset_name = self.dataset_config[split]["query"][0]
+        dataset_name = self.datasets[split]["query"].name
         if self.retriever !=  None:
             ranking_file = f'{self.run_folder}/run.retrieve.top_{self.retriever.top_k_documents}.{dataset_name}.{split}.{self.retriever.get_model_name()}.trec'
             if not os.path.exists(ranking_file):
@@ -107,7 +107,6 @@ class RAG:
                 write_trec(reranking_file, query_ids, doc_ids, scores)
             else:
                 query_ids, doc_ids, scores = load_trec(reranking_file)
-
         if self.generator != None: 
             gen_dataset = make_hf_dataset(
                 self.datasets[split], 
@@ -124,6 +123,7 @@ class RAG:
                 references=labels, 
                 questions=queries
                 )
+            print(metrics_out)
             out_folder = f'{self.experiment_folder}/{self.run_name}/'
             write_generated(
                 out_folder,
@@ -136,6 +136,8 @@ class RAG:
                 )
 
             print_generate_out(
+                queries,
+                instructions,
                 responses,
                 query_ids, 
                 labels)